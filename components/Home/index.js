b32d8620e582ec99272fca5f770b510c1782de54
preveenraj <97preveenraj@gmail.com>
2021-07-18 01:43:50 +0530

    add profile banner

--- components/Home/index.js
+++ components/Home/index.js
@@ -1,9 +1,22 @@
 import React from 'react'
-
+import Image from 'next/image';
 const Home = () => {
   return (
-    <div className="font-bold flex justify-center items-center h-screen bg-blue-500 p-2">
-      <p className="text-7xl text-gray-900">Preveen Raj</p>
+    <div className="font-bold flex flex-col gap-4 justify-center items-center h-screen bg-white p-2">
+      <div className="h-1/2 w-5/12 relative rounded-lg">
+        <Image
+          src="/profile_banner.jpg"
+          alt="Picture of the author"
+          className="rounded-lg"
+          layout="fill"
+          // width={500} automatically provided
+          // height={500} automatically provided
+          // blurDataURL="data:..." automatically provided
+          // Optionally allows to add a blurred version of the image while loading
+          placeholder="blur"
+        />
+      </div>
+      <p className="text-7xl text-gray-700">Preveen Raj</p>
     </div>
   )
 }
