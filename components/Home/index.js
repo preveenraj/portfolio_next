fb944de07e2f18bb09531aa5b59cf730c7e222d5
preveenraj <97preveenraj@gmail.com>
2021-07-18 01:52:00 +0530

    add banner image

--- components/Home/index.js
+++ components/Home/index.js
@@ -2,21 +2,19 @@ import React from 'react'
 import Image from 'next/image';
 const Home = () => {
   return (
-    <div className="font-bold flex flex-col gap-4 justify-center items-center h-screen bg-white p-2">
-      <div className="h-1/2 w-5/12 relative rounded-lg">
+    <div className="font-bold flex flex-col gap-2 justify-center items-center h-screen bg-gray-100 p-2">
+      <div className="h-1/2 w-full md:w-4/6 relative">
         <Image
           src="/profile_banner.jpg"
           alt="Picture of the author"
-          className="rounded-lg"
-          layout="fill"
-          // width={500} automatically provided
-          // height={500} automatically provided
-          // blurDataURL="data:..." automatically provided
-          // Optionally allows to add a blurred version of the image while loading
-          placeholder="blur"
+          className="rounded-lg shadow"
+          layout="responsive"
+          priority={true}
+          width={1920}
+          height={1080}
         />
       </div>
-      <p className="text-7xl text-gray-700">Preveen Raj</p>
+      <p className="text-7xl text-gray-600">Preveen Raj</p>
     </div>
   )
 }
