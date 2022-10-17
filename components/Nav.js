af259a65630c0716d85bbb62efa62c84e49886a4
Preveen Raj <97preveenraj@gmail.com>
2022-10-17 15:49:09 +0000

    commeny work and experience for now

--- components/Nav.js
+++ components/Nav.js
@@ -12,12 +12,12 @@ const Nav = ({
       <nav className="h-12 absolute top-0 right-0 w-full z-50">
         <div className="h-full flex items-center justify-center md:justify-end font-bold text-xl text-gray-100 p-10">
           <div className="flex gap-4">
-            <span
+            {/* <span
               onClick={() => setTab("experience")}
               className="cursor-pointer">EXP</span>
             <span
               onClick={() => setTab("projects")}
-              className="cursor-pointer">WORK</span>
+              className="cursor-pointer">WORK</span> */}
             <a 
             href="/preveen-raj-resume.pdf" 
             className="block md:hidden"
